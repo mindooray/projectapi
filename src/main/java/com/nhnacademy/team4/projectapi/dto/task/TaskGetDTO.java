6bb258c973bf44ff73b2867e2b956d3e74f94451
lightbell03 <bellight28@gmail.com>
2023-06-12 14:44:48 +0900

    feat: add

--- src/main/java/com/nhnacademy/team4/projectapi/dto/task/TaskGetDTO.java
+++ src/main/java/com/nhnacademy/team4/projectapi/dto/task/TaskGetDTO.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.team4.projectapi.dto.task;
 
 
+import com.nhnacademy.team4.projectapi.entity.Tag;
 import com.nhnacademy.team4.projectapi.entity.Task;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
