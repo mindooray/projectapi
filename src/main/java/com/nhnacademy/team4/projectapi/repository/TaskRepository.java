3b31d4b86b8367241a4d5bc874f3500e02c8ff4c
Dod93 <ehdgus3507@naver.com>
2023-06-14 13:49:23 +0900

    TaskServiceTest ADD

--- src/main/java/com/nhnacademy/team4/projectapi/repository/TaskRepository.java
+++ src/main/java/com/nhnacademy/team4/projectapi/repository/TaskRepository.java
@@ -1,8 +1,7 @@
 package com.nhnacademy.team4.projectapi.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.nhnacademy.team4.projectapi.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
--- src/test/java/com/nhnacademy/team4/projectapi/service/TaskServiceTest.java
+++ src/test/java/com/nhnacademy/team4/projectapi/service/TaskServiceTest.java
@@ -1,6 +1,8 @@
 package com.nhnacademy.team4.projectapi.service;
 
 import com.nhnacademy.team4.projectapi.dto.task.TaskPostDTO;
+import com.nhnacademy.team4.projectapi.dto.task.TaskTitleListDTO;
+import com.nhnacademy.team4.projectapi.dto.task.TaskUpdateDTO;
 import com.nhnacademy.team4.projectapi.entity.Project;
 import com.nhnacademy.team4.projectapi.entity.Tag;
 import com.nhnacademy.team4.projectapi.entity.Task;
@@ -16,12 +18,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TaskServiceTest {
@@ -71,12 +75,11 @@ class TaskServiceTest {
 
         Assertions.assertThat(task).isEqualTo(actual);
     }
+
     @Test
     void createTaskWhenNo() {
         Project project = new Project();
         project.setProjectId(1L);
-
-
         TaskPostDTO dto = new TaskPostDTO();
         dto.setAccountId(1L);
         dto.setTitle("title");
@@ -107,17 +110,110 @@ class TaskServiceTest {
 
     @Test
     void getAllTask() {
+        // Mock 데이터 설정
+        List<Task> tasks = new ArrayList<>();
+        Task task1 = new Task();
+        task1.setTaskId(1L);
+        task1.setAccountId(1L);
+        task1.setTitle("Task 1");
+        task1.setProject(new Project());
+        task1.setStatus(TaskStatus.IN_PROGRESS);
+
+        Task task2 = new Task();
+        task2.setTaskId(2L);
+        task2.setAccountId(1L);
+        task2.setTitle("Task 2");
+        task2.setProject(new Project());
+        task2.setStatus(TaskStatus.IN_PROGRESS);
+
+        tasks.add(task1);
+        tasks.add(task2);
+
+        // Mock 객체 및 응답 값 설정
+        given(taskRepository.findAll()).willReturn(tasks);
+
+        // 테스트 수행
+        List<TaskTitleListDTO> taskTitleList = taskService.getAllTask();
+
+        // 결과 검증
+        Assertions.assertThat(taskTitleList).hasSize(2);
+        Assertions.assertThat(taskTitleList.get(0).getTaskId()).isEqualTo(task1.getTaskId());
+        Assertions.assertThat(taskTitleList.get(0).getTitle()).isEqualTo(task1.getTitle());
+        Assertions.assertThat(taskTitleList.get(1).getTaskId()).isEqualTo(task2.getTaskId());
+        Assertions.assertThat(taskTitleList.get(1).getTitle()).isEqualTo(task2.getTitle());
     }
 
     @Test
     void getTask() {
+        // Mock 데이터 설정
+        Task task = new Task();
+        task.setTaskId(1L);
+        task.setTitle("Task");
+        task.setAccountId(1L);
+        task.setProject(new Project());
+        task.setStatus(TaskStatus.IN_PROGRESS);
+
+        // Mock 객체 및 응답 값 설정
+        given(taskRepository.findById(anyLong())).willReturn(Optional.of(task));
+
+        // 테스트 수행
+        Task actual = taskService.getTask(1L);
+
+        // 결과 검증
+        Assertions.assertThat(actual).isEqualTo(task);
     }
 
     @Test
     void updateTask() {
+        // Mock 데이터 설정
+        TaskUpdateDTO taskDTO = new TaskUpdateDTO();
+        taskDTO.setTitle("Updated Task");
+        taskDTO.setContent("Updated Content");
+        taskDTO.setStatus("COMPLETED");
+
+        Task existingTask = new Task();
+        existingTask.setTaskId(1L);
+        existingTask.setTitle("Task");
+        existingTask.setContent("Content");
+        existingTask.setStatus(TaskStatus.IN_PROGRESS);
+        existingTask.setModifyDate(LocalDateTime.now());
+
+        Task updatedTask = new Task();
+        updatedTask.setTaskId(1L);
+        updatedTask.setTitle("Updated Task");
+        updatedTask.setContent("Updated Content");
+        updatedTask.setStatus(TaskStatus.DONE);
+        updatedTask.setModifyDate(LocalDateTime.now());
+
+        // Mock 객체 및 응답 값 설정
+        given(taskService.getTask(anyLong())).willReturn(existingTask);
+        given(taskRepository.save(any(Task.class))).willReturn(updatedTask);
+
+        // 테스트 수행
+        Task actual = taskService.updateTask(1L, taskDTO);
+
+        // 결과 검증
+        Assertions.assertThat(actual).isEqualTo(updatedTask);
     }
 
     @Test
     void deleteTask() {
+        Task task = new Task();
+        task.setTaskId(1L);
+        doNothing().when(taskRepository).delete(any());
+        given(taskRepository.findById(anyLong())).willReturn(Optional.of(task));
+
+        taskService.deleteTask(1L);
+        verify(taskRepository, times(1)).delete(any());
     }
+
+    @Test
+    void deleteTaskNotFoundId() {
+        given(taskRepository.findById(anyLong())).willReturn(Optional.empty());
+
+        Assertions.assertThatThrownBy(() -> taskService.deleteTask(1L))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Task not found with ID: 1");
+    }
+
 }
